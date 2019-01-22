8387c48c30d4aef7b115a36f353954865ad76e75
지나가던 잉여 <producer82@naver.com>
2019-01-22 18:07:39 +0900

    개발 시작 2

--- Bootloader/bootloader.asm
+++ Bootloader/bootloader.asm
@@ -2,7 +2,7 @@
 ;			Infinite Synthesis x64			;
 ;											;
 ;파일 명: bootloader.asm						;
-;설명: Infinite Synthesis의 부트로더			;
+;설명: Infinite Synthesis의 기본 부트로더		;
 ;최초 작성: 2019-01-22 						;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -11,11 +11,24 @@
 
 section .text
 
-jmp 0x07C0:START	
+jmp 0x07C0:MAIN
 
-START:
+;메인 함수
+MAIN:
 	mov ax, cs	; DS = CS
 	mov ds, ax
 	xor ax, ax
 	
+	jmp $
+
+;510 바이트까지 제로 필
+times 510 - ($ - $$) db 0x00
+
+;부트로더 시그니쳐
+db 0x55
+db 0xAA 
+	
+
+	
+
 	
