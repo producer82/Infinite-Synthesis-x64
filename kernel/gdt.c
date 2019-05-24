8d782a5939d90b7f5deb97859a7575ad4a229bbd
지나가던 잉여 <producer82@naver.com>
2019-05-24 21:43:42 +0900

    Add Null Descriptor

--- kernel/gdt.c
+++ kernel/gdt.c
@@ -1,17 +1,23 @@
 #include "descriptor.h"
 
+#define NUMBER_OF_ENTRYS 3
+
 void initGDT(){
-	int numberOfEntry;
+	GDTR *gdtr = (unsigned long*)0x150000;
+	GDT *gdtEntry = (unsigned long*)(0x150000 + (sizeof(GDTR)));
 
-	GDT *gdtEntry = (unsigned long*)0x105000;
-	GDTR *gdtr = (unsigned long*)(0x105000 + sizeof(gdtEntry) * numberOfEntry);
-
-	gdtr->size = sizeof(gdtEntry) * numberOfEntry - 1;
+	gdtr->size = sizeof(gdtEntry) * NUMBER_OF_ENTRYS - 1;
 	gdtr->offset = gdtEntry;
-
 	
+	//Null Descriptor 생성
+	setGDTEntry(gdtEntry[0], 0, 0, 0, 0);
 }
 
-void setGDTEntry(GDT *entry, unsigned long* baseAddress, unsigned long* upperAddress, unsigned char flag){
-
+void setGDTEntry(GDT *entry, unsigned long* baseAddress, unsigned long* limitAddress, unsigned char accessByte, unsigned char flags){
+	entry->lowerLimitAddress = limitAddress & 0xFFFF;
+	entry->lowerBaseAddress = baseAddress & 0xFFFF;
+	entry->middleBaseAddress = (baseAddress >> 16) & 0xFF;
+	entry->accessByte = accessByte;
+	entry->higherLimitAndFlags = ((limitAddress >> 16) & 0xF) | flags;
+	entry->higherBaseAddress = (baseAddress >> 24) & 0xFF;
 }
