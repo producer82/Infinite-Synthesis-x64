e33371cc8b582e158ec87e1436d0ff7d46b05247
지나가던 잉여 <producer82@naver.com>
2019-07-26 19:18:54 +0900

    HDD Driver

--- kernel/drivers/hdd.c
+++ kernel/drivers/hdd.c
@@ -8,6 +8,55 @@ void writeSector(){
 	
 }
 
-char isRdy(){
+char readStatusRegister(){
+	unsigned char statusRegister = 0;
 	
+	__asm__ __volatile__(
+		"in ax, 0x1F7;"
+		"mov %0, ax"::"=r"(statusRegister);
+	);
+	
+	return statusRegister;
+}
+
+void cacheFlush(){
+	__asm__ __volatile__(
+		"out 0x1F7, 0xE7;"
+	);
+}
+
+char isErr(){
+	if(statusRegister() & 0x1) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+char isDrq(){
+	if((statusRegister() >> 3) & 0x1) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+char isRdy(){
+	if((statusRegister() >> 6) & 0x1) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+char isBsy(){
+	if((statusRegister() >> 7) & 0x1) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
 }
